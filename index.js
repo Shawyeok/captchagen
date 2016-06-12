ba4e8a12dcb37d5aecbe0a8111767d9154950e56
sucheng.wang <sucheng.wang@zhaopin.com.cn>
2016-06-12 19:33:57 +0800

    support custom font size and the number of lines and first commit

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var drawBackground = require('./lib/middle/drawBackground');
 var drawText = require('./lib/middle/drawText');
 var drawLines = require('./lib/middle/drawLines');
 
-var getColors = require('./lib/getColors');
-var getFontSize = require('./lib/getFontSize');
-var generateText = require('./lib/generateText');
-
 module.exports = {
   Captcha: Captcha,
 
@@ -14,11 +10,6 @@ module.exports = {
   drawBackground: drawBackground,
   drawText: drawText,
   drawLines: drawLines,
-
-  // utils
-  getColors: getColors,
-  getFontSize: getFontSize,
-  generateText: generateText,
   
   // use default settings
   create: function (opt) {
@@ -26,7 +17,6 @@ module.exports = {
     cap.use(drawBackground);
     cap.use(drawLines);
     cap.use(drawText);
-    cap.use(drawLines);
     return cap;
   }
 };
